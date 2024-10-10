6f7b68f90a9b375b777b9ead9f528d9496d23d38
KevinGachiri <96230837+KevinGachiri@users.noreply.github.com>
2024-10-10 11:39:44 +0300

    Add files via upload

---
+++ List_Tuple_Dictionary.py
@@ -0,0 +1,38 @@
+#List
+myFruitList = ["apple", "banana", "cherry"]
+print(myFruitList)
+print(type(myFruitList))
+
+print(myFruitList[0])
+print(myFruitList[1])
+print(myFruitList[2])
+
+myFruitList[2] = "orange"
+print(myFruitList)
+
+
+#Tuple
+myFinalAnswer = ("apple", "banana", "pinneapple")
+print(myFinalAnswer)
+print(type(myFinalAnswer))
+
+print(myFinalAnswer[0])
+print(myFinalAnswer[1])
+print(myFinalAnswer[2])
+
+
+#Dictionary
+myFavoriteFruitDictionary = {
+    "Akua" : "apple",
+    "Saanvi" : "banana",
+    "Paulo" : "pinneapple"
+}
+print(myFavoriteFruitDictionary)
+print(type(myFavoriteFruitDictionary))
+
+print(myFavoriteFruitDictionary["Akua"])
+print(myFavoriteFruitDictionary["Saanvi"])
+print(myFavoriteFruitDictionary["Paulo"])
+
+
+
